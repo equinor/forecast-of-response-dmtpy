0166bb9dfcfb9fa599eac190760f478ed1b2e811
Timothy Edward Kendon <TIKE@equinor.com>
2021-12-29 16:13:52 +0100

    corrected error in boolean casting

--- ForApp/io/simulationconfig.py
+++ ForApp/io/simulationconfig.py
@@ -102,6 +102,7 @@ class SimulationConfig(Entity):
         """Set published"""
         self.__published = bool(value)
 
+    @staticmethod
     def _str2bool(v): return v.lower() in ("yes", "true", "t", "1") or v.lower() in "true"
 
     def get(self, name:Optional[str]=None, delimiter:str=';') -> Union[List[VariableRun],str]:
@@ -123,7 +124,7 @@ class SimulationConfig(Entity):
         TODO: compile functionality for comparisonGroups / bar-plots e.g. "heading variation plots"
                     
         """
-        cast = {'string':str, 'number':float, 'integer':int, 'bool': _str2bool}
+        cast = {'string':str, 'number':float, 'integer':int, 'bool': self._str2bool}
         iterables, variableRuns = [], []
         iterable_names = []
         for variable in self.variables:
